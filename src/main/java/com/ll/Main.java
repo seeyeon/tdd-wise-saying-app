3914264abbc06ea3fad81b9d388fc5adae96952a
seeyeon <gulf72pr@swu.ac.kr>
2024-11-26 12:25:13 +0900

    person enter

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,8 +1,26 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        lab1();
+        
+        lab2();
+    }
+
+    private static void lab2() {
+    }
+
+    private static void lab1() {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("명령) ");
+        String cmd = sc.nextLine().trim();
+
+        System.out.println("입력한 명령: " +  cmd);
+    
     }
 }
